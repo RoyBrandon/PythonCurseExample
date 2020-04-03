e8b8cc608f34622e4c22a4a8524c66ae4a6bfb54
Roy Brandon <lroylbrandonl36963l@gmail.com>
2020-04-03 11:02:51 -0600

    actualizacion xd

--- iva.py
+++ iva.py
@@ -4,5 +4,7 @@
 
 print('Calculo de IVA')
 precio = float(input('Ingresa el precio del producto: '))
-calculoIva = precio * 1.16
-print('El precio del producto con IVA es: ', calculoIva)
+calculo_total_iva = precio * 1.16
+iva = precio * 0.16
+print('El IVA del producto es ', iva )
+print('El precio del producto con IVA es: ','{:0.2f}'.format(calculo_total_iva))
